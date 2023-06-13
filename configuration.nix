14009c165afebf5b64425378c126eb8209441cd6
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-06-13 23:39:33 +0200

    Switch to home manager for user packages

--- configuration.nix
+++ configuration.nix
@@ -36,10 +36,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Enable network manager
-  networking.networkmanager = {
-    enable = true;
-    wifi.macAddress = "random";
-  };
+  networking.networkmanager.enable = true;
 
   # Set time zone
   time.timeZone = "Europe/Zurich";
@@ -54,101 +51,9 @@
   users.users.lena = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
-    packages = with pkgs; [
-      alacritty
-      autotiling-rs
-      awscli2
-      brave
-      brightnessctl
-      cargo
-      chafa
-      chromium
-      clipman
-      diff-so-fancy
-      dig
-      fd
-      fx
-      fzf
-      gammastep
-      gh
-      gimp
-      go
-      golangci-lint
-      gopass
-      grim
-      gron
-      hugo
-      imv
-      inkscape
-      jpegoptim
-      jq
-      kanshi
-      kubectl
-      kubectx
-      libreoffice-fresh
-      lolcat
-      lutris
-      lynis
-      mako
-      moq
-      mpv
-      ncdu
-      nmap
-      nodejs
-      stern
-      nodePackages.prettier
-      nodePackages.svgo
-      optipng
-      playerctl
-      poetry
-      protobuf
-      pwgen
-      python3
-      python311Packages.flake8
-      quickemu
-      realvnc-vnc-viewer
-      ripgrep
-      rnix-lsp
-      rustc
-      shellcheck
-      shfmt
-      signal-desktop
-      slurp
-      stow
-      stylua
-      swaybg
-      swayidle
-      swaylock
-      taskwarrior
-      terraform
-      tflint
-      timewarrior
-      tldr
-      tmate
-      tree
-      vifm
-      waybar
-      wf-recorder
-      whois
-      wine
-      wl-clipboard
-      wofi
-      workstyle
-      zathura
-      zeroad
-      zoxide
-    ];
     password = "changeme";
   };
   users.defaultUserShell = pkgs.zsh;
-
-  fonts.fonts = with pkgs; [
-    fira-mono
-    lato
-    noto-fonts-cjk
-    noto-fonts-emoji
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
-  ];
 
   environment.systemPackages = with pkgs; [
     curl
@@ -171,14 +76,8 @@
   # Enable ZSH
   programs.zsh.enable = true;
 
-  # Enable system monitoring
+  # Enable process viewer
   programs.htop.enable = true;
-
-  # Enable password manager
-  programs.browserpass.enable = true;
-
-  # Enable KDE Connect
-  programs.kdeconnect.enable = true;
 
   # Enable terminal multiplexer
   programs.tmux.enable = true;
@@ -190,13 +89,6 @@
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   programs.sway.enable = true;
   xdg.portal.wlr.enable = true;
-
-  # Enable GPG agent
-  programs.gnupg.agent.enable = true;
-
-  # Enable Kubernetes
-  services.k3s.enable = true;
-  networking.firewall.allowedTCPPorts = [ 6643 ];
 
   # Enable firmware updater
   services.fwupd.enable = true;
