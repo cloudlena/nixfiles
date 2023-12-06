9f47c522c161d93ed06321888ddc39934770aec9
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-12-06 23:18:29 +0100

    Update nixpkgs input

--- home/default.nix
+++ home/default.nix
@@ -15,7 +15,7 @@
   home.username = "lena";
   home.homeDirectory = "/home/${config.home.username}";
 
-  home.stateVersion = "23.05";
+  home.stateVersion = "23.11";
 
   # Let Home Manager install and manage itself
   programs.home-manager.enable = true;
